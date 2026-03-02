48022a4e3cbdb1540fa4d57dcfae0f74844e4d39
Didar <didar.dildakhan.@narxoz.kz>
2026-03-02 23:00:52 +0500

    9 commit

--- homework-rpg-3-main/src/com/narxoz/rpg/Main.java
+++ homework-rpg-3-main/src/com/narxoz/rpg/Main.java
@@ -1,7 +1,22 @@
 package com.narxoz.rpg;
 
+import com.narxoz.rpg.adapter.EnemyCombatantAdapter;
+import com.narxoz.rpg.adapter.HeroCombatantAdapter;
+import com.narxoz.rpg.battle.BattleEngine;
+import com.narxoz.rpg.enemy.Goblin;
+import com.narxoz.rpg.hero.Warrior;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("RPG Game started...");
+
+        Warrior warrior = new Warrior("Arthas");
+        Goblin goblin = new Goblin();
+
+        HeroCombatantAdapter heroAdapter = new HeroCombatantAdapter(warrior);
+        EnemyCombatantAdapter enemyAdapter = new EnemyCombatantAdapter(goblin);
+
+        BattleEngine engine = new BattleEngine();
+        engine.fight(heroAdapter, enemyAdapter);
     }
 }
